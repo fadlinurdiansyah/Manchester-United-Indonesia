09026670e44e26e52670b47ffbccbf7b73def6b6
NDS <nds@ndss-macbook.local>
2019-02-27 03:37:41 +0700

    Initial Commit

---
+++ Manchester United Indonesia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Manchester United Indonesia
+//
+//  Created by NDS on 27/02/19.
+//  Copyright © 2019 NDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
